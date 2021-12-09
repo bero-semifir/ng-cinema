7677ff002ea7fbfcc5034cf32c7213ed5def6e93
Administrator <admin@example.com>
2021-12-09 11:50:14 +0100

    Ajout de la navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmPageComponent } from './film-page/film-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo: 'films', pathMatch: 'full'},
+  {path: "films", component: FilmPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
